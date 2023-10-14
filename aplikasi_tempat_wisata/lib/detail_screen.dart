bc61d46b0e62e2875998c2ce238f337016561818
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-15 06:47:05 +0700

    Add padding to listview

--- aplikasi_tempat_wisata/lib/detail_screen.dart
+++ aplikasi_tempat_wisata/lib/detail_screen.dart
@@ -62,11 +62,20 @@ class DetailScreen extends StatelessWidget {
               Container(
                 height: 150,
                 child: ListView(
+                  scrollDirection: Axis.horizontal,
                   children: [
-                    Image.network(
-                    'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
-                    Image.network('https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
-                    Image.network('https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg')
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Image.network('https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Image.network('https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Image.network('https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),
+                    )
                   ],
                 ),
               ),
