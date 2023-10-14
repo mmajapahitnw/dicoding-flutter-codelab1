a3178387800a07d8f2f6609cca4de62b3365f70d
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-15 06:30:10 +0700

    Relocate DetailScreen class into new dart file

--- aplikasi_tempat_wisata/lib/detail_screen.dart
+++ aplikasi_tempat_wisata/lib/detail_screen.dart
@@ -1,2 +1,65 @@
 import 'package:flutter/material.dart';
 
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch, //to match column width with device's width
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(top: 16),
+              child: const Text(
+                  'Farm House Lembang',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  )),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: const <Widget>[
+                      Icon(Icons.calendar_today),
+                      SizedBox(height: 8,),
+                      Text('Open Everyday'),
+                    ],
+                  ),
+                  Column(
+                    children: const <Widget>[
+                      Icon(Icons.access_time_rounded),
+                      SizedBox(height: 8,),
+                      Text('09.00 - 20.00')
+                    ],
+                  ),
+                  Column(
+                    children: const <Widget>[
+                      Icon(Icons.monetization_on_rounded),
+                      SizedBox(height: 8,),
+                      Text('Rp 25.000')
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+                padding: const EdgeInsets.all(16),
+                child: const Text(
+                  'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16),
+                )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
