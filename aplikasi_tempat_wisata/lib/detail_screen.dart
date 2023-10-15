dc71d50721318e6ef022d2d02ba9ca3684349364
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-15 08:14:19 +0700

    Add rounded corner to image carrousel

--- aplikasi_tempat_wisata/lib/detail_screen.dart
+++ aplikasi_tempat_wisata/lib/detail_screen.dart
@@ -66,15 +66,21 @@ class DetailScreen extends StatelessWidget {
                   children: [
                     Padding(
                       padding: const EdgeInsets.all(4.0),
-                      child: Image.network('https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network('https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg')),
                     ),
                     Padding(
                       padding: const EdgeInsets.all(4.0),
-                      child: Image.network('https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network('https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg')),
                     ),
                     Padding(
                       padding: const EdgeInsets.all(4.0),
-                      child: Image.network('https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network('https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg')),
                     )
                   ],
                 ),
