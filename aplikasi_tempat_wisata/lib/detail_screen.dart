5e0ed702ed6e373507bfa9132e7fe7ecf8229bef
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-15 08:19:44 +0700

    Add fonts assets

--- aplikasi_tempat_wisata/lib/detail_screen.dart
+++ aplikasi_tempat_wisata/lib/detail_screen.dart
@@ -68,19 +68,22 @@ class DetailScreen extends StatelessWidget {
                       padding: const EdgeInsets.all(4.0),
                       child: ClipRRect(
                           borderRadius: BorderRadius.circular(10),
-                          child: Image.network('https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg')),
+                          child: Image.network('https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg')
+                      ),
                     ),
                     Padding(
                       padding: const EdgeInsets.all(4.0),
                       child: ClipRRect(
                           borderRadius: BorderRadius.circular(10),
-                          child: Image.network('https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg')),
+                          child: Image.network('https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg')
+                      ),
                     ),
                     Padding(
                       padding: const EdgeInsets.all(4.0),
                       child: ClipRRect(
                           borderRadius: BorderRadius.circular(10),
-                          child: Image.network('https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg')),
+                          child: Image.network('https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg')
+                      ),
                     )
                   ],
                 ),
