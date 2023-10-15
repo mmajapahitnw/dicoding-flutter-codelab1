c9179211560156364848fb1138908539a0f512e7
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-15 08:31:12 +0700

    Remove safe area

--- aplikasi_tempat_wisata/lib/detail_screen.dart
+++ aplikasi_tempat_wisata/lib/detail_screen.dart
@@ -8,102 +8,101 @@ class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch, //to match column width with device's width
-            children: <Widget>[
-              Image.asset('images/farm-house.jpg'),
-              Container(
-                margin: const EdgeInsets.only(top: 16),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch, //to match column width with device's width
+          children: <Widget>[
+            Image.asset('images/farm-house.jpg'),
+            Container(
+              margin: const EdgeInsets.only(top: 16),
+              child: const Text(
+                  'Farm House Lembang',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Staatliches',
+                  )),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      const Icon(Icons.calendar_today),
+                      const SizedBox(height: 8,),
+                      Text(
+                        'Open Everyday',
+                        style: informationTextStyle,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      const Icon(Icons.access_time_rounded),
+                      const SizedBox(height: 8,),
+                      Text(
+                        '09.00 - 20.00',
+                        style: informationTextStyle,
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      const Icon(Icons.monetization_on_rounded),
+                      const SizedBox(height: 8,),
+                      Text(
+                        'Rp 25.000',
+                        style: informationTextStyle,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+                padding: const EdgeInsets.all(16),
                 child: const Text(
-                    'Farm House Lembang',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Staatliches',
-                    )),
-              ),
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 16),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        const Icon(Icons.calendar_today),
-                        const SizedBox(height: 8,),
-                        Text(
-                          'Open Everyday',
-                          style: informationTextStyle,
-                        ),
-                      ],
+                  'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16),
+                )
+            ),
+            Container(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network('https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg')
                     ),
-                    Column(
-                      children: <Widget>[
-                        const Icon(Icons.access_time_rounded),
-                        const SizedBox(height: 8,),
-                        Text(
-                          '09.00 - 20.00',
-                          style: informationTextStyle,
-                        )
-                      ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network('https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg')
                     ),
-                    Column(
-                      children: <Widget>[
-                        const Icon(Icons.monetization_on_rounded),
-                        const SizedBox(height: 8,),
-                        Text(
-                          'Rp 25.000',
-                          style: informationTextStyle,
-                        )
-                      ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network('https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg')
                     ),
-                  ],
-                ),
-              ),
-              Container(
-                  padding: const EdgeInsets.all(16),
-                  child: const Text(
-                    'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 16),
                   )
+                ],
               ),
-              Container(
-                height: 150,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.network('https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg')
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.network('https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg')
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.network('https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg')
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
+
     );
   }
 }
