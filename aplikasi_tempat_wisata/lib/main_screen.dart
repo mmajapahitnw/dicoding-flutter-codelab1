37e985da48a9be6b1af56be77ea8eb96c6218ae6
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-16 08:46:59 +0700

    Create appBar for main screen

--- aplikasi_tempat_wisata/lib/main_screen.dart
+++ aplikasi_tempat_wisata/lib/main_screen.dart
@@ -5,6 +5,10 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Wisata Bandung'),
+      ),
+    );
   }
 }
