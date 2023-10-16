d209156a1657dc064ecd70d36f1d02f9f94b7df2
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-16 08:58:15 +0700

    Wrap the row whildren in expanded widget

--- aplikasi_tempat_wisata/lib/main_screen.dart
+++ aplikasi_tempat_wisata/lib/main_screen.dart
@@ -9,30 +9,39 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Wisata Bandung'),
       ),
-      body: Card(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Image.asset('images/farm-house.jpg'),
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Text(
-                    'Farm House Lembang',
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text('Lembang'),
-                ],
+      body: InkWell(
+        onTap: () {},
+        child: Card(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                  flex: 1,
+                  child: Image.asset('images/farm-house.jpg')
+              ),
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(
+                        'Farm House Lembang',
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text('Lembang'),
+                    ],
+                  )
+                ),
               )
-            )
-          ],
-        )
+            ],
+          )
+        ),
       )
     );
   }
