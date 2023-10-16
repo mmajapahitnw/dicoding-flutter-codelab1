fafae5ca398c7760a3e3744ee4c1c13bc43cedd5
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-16 09:51:14 +0700

    Update main_screen.dart

--- aplikasi_tempat_wisata/lib/main_screen.dart
+++ aplikasi_tempat_wisata/lib/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:aplikasi_tempat_wisata/detail_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:aplikasi_tempat_wisata/model/tourism_place.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -10,43 +11,49 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Wisata Bandung'),
       ),
-      body: InkWell(
-        onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return DetailScreen();
-          }));
-        },
-        child: Card(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                  flex: 1,
-                  child: Image.asset('images/farm-house.jpg')
-              ),
-              Expanded(
-                flex: 2,
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Text(
-                        'Farm House Lembang',
-                        style: const TextStyle(fontSize: 16),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Text('Lembang'),
-                    ],
+      body: ListView.builder(
+        itemCount: tourismPlaceList.length,
+        itemBuilder: (context, index) {
+          final TourismPlace place = tourismPlaceList[index];
+          return InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return DetailScreen();
+              }));
+            },
+            child: Card(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Image.asset(place.imageAsset)
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                            place.name,
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(place.location),
+                        ],
+                      )
+                    ),
                   )
-                ),
+                ],
               )
-            ],
-          )
-        ),
+            ),
+          );
+        }
       )
     );
   }
