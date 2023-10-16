a66a9995dc6bc1ede381db396272e1747d77a849
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-16 15:27:25 +0700

    Update main_screen.dart

--- aplikasi_tempat_wisata/lib/main_screen.dart
+++ aplikasi_tempat_wisata/lib/main_screen.dart
@@ -9,7 +9,7 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Wisata Bandung. Size: ${MediaQuery.of(context).size.width}'),
+        title: Text('Wisata Bandung'),
       ),
       body: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
@@ -89,6 +89,8 @@ class TourismPlaceGrid extends StatelessWidget {
       padding: const EdgeInsets.all(24),
       child: GridView.count(
         crossAxisCount: gridCount,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
         children: tourismPlaceList.map((place) {
           return InkWell(
             onTap: () {
